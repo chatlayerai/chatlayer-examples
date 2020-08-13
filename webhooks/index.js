2fefd27137ed5e984e8ba4f38240e5c4e78afe66
Mahen Vermeulen <info@mahenvermeulen.com>
2020-08-13 12:18:49 +0200

    simple webhook logger

--- webhooks/index.js
+++ webhooks/index.js
@@ -1,11 +1,12 @@
-require('')
-import express from 'express';
-import bunyan from 'bunyan';
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.post('/messages', (req, res) => {
-
+  console.log(JSON.stringify(req.body, null, 4));
+  res.sendStatus(200);
 });
 
-app.listen(process.env)
+app.listen(9003, () => console.log('RUNNING'))
