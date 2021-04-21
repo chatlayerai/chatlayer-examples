f18df2570168301069f1bed0a868c54d4bf82178
mahenvermeulen <info@mahenvermeulen.com>
2021-04-21 16:06:02 +0200

    expand offloading webhook example

--- offloading-webhook/index.js
+++ offloading-webhook/index.js
@@ -9,15 +9,31 @@ app.get('/', (req, res) => {
   res.status(200).json({ "status": "ok" });
 });
 
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return date.toISOString().replace('.*T', '');
+}
 app.post('/', (req, res) => {
   if (req.body.event === 'messages') {
     console.log('MESSAGE DETECTED');
-    console.log(JSON.stringify(req.body, null, 4));
+    // console.log(JSON.stringify(req.body, null, 4));
+    const { messages, ...rest } = req.body;
+    console.log('Incoming %s event -> %j', rest.event, rest);
+    for (const { actor, message, timestamp } of messages) {
+      const time = formatDate(timestamp);
+      console.log('%s | %s said: %j', `${time}`, actor, message);
+    }
   }
 
   if (req.body.event === 'offload') {
     console.log('OFFLOAD DETECTED.')
-    console.log(JSON.stringify(req.body, null, 4));
+    // console.log(JSON.stringify(req.body, null, 4));
+    const { transcript, ...rest } = req.body;
+    console.log('Incoming %s event -> %j', rest.event, rest);
+    for (const { actor, message, timestamp } of transcript) {
+      const time = formatDate(timestamp);
+      console.log('%s | %s said: %j', `${time}`, actor, message);
+    }
   }
   res.sendStatus(200);
 });
